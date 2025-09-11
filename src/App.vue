32b7452d79248b23ccd91b8325c7b1508e8dc145
Bertrand Rock <bertrand.rock@iut-tlse3.fr>
2025-09-11 11:50:23 +0200

    portail re fonctionnel

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supaBaseClient'
 import '@/assets/style.css'
@@ -6,25 +7,44 @@ import '@/assets/style.css'
 
 const applications = ref([])
 
-
 async function getApplications() {
   const { data } = await supabase.from('applications').select()
   applications.value = data
 }
 
 onMounted(() => {
-   getApplications()
+  getApplications()
 })
 
 </script>
 
 <template>
+
   <div class="main-container">
+
+      <Auth></Auth>
+
     <h1>Portail des applications TICE</h1>
 
     <h2>Plateformes utilisateurs</h2>
+
     <div id="services-users">
-      <a v-for="app in applications.filter(app => app.useradmin)" 
+
+      <a v-for="app in applications.filter(app => app.useradmin).sort((a, b) => a.title.localeCompare(b.title))"
+         :key="app.id"
+         :href="app.url"
+         class="service">
+        <img :src="app.img">
+        <span>{{ app.title }}</span>
+      </a>
+
+    </div>
+
+    <h2>Plateformes administration</h2>
+
+    <div id="services-admin">
+
+      <a v-for="app in applications.filter(app => !app.useradmin).sort((a, b) => a.title.localeCompare(b.title))"
          :key="app.id"
          :href="app.url"
          class="service">
@@ -33,15 +53,6 @@ onMounted(() => {
       </a>
     </div>
 
-    <h2>Plateformes administration</h2>
-    <div id="services-admin">
-      <a v-for="app in applications.filter(app => !app.useradmin)" 
-         :key="app.id"
-         :href="app.url"
-         class="service">
-        <img :src="app.img">
-        <span>{{ app.title }}</span>
-      </a>
-    </div>
   </div>
+
 </template>
